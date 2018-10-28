75ee1eec8cb650433627caa343103fb33a7f8a9f
galipremsagar <sagarprem75@gmail.com>
2018-10-28 13:26:55 -0400

    Revert "Initial commit"
    This reverts commit 4a99a9e35aa25aa280626677f8b5bb2973d7ce39.

--- data/get_dataset.sh
+++
@@ -1,6 +0,0 @@
-# Get GTSDB
-#!/bin/bash
-curl http://benchmark.ini.rub.de/Dataset_GTSDB/TestIJCNN2013.zip -o TestIJCNN2013.zip
-tar -xzvf TestIJCNN2013.zip
-curl http://benchmark.ini.rub.de/Dataset_GTSDB/TrainIJCNN2013.zip -o TrainIJCNN2013.zip
-tar -xzvf TrainIJCNN2013.zip
